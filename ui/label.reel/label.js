58cbd84fa56ffd888fe46a128c429dc98298446b
noochie <noreply>
2016-12-15 14:54:02 +0000

    Add component label

---
+++ ui/label.reel/label.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/label.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Label
+ * @extends Component
+ */
+exports.Label = Component.specialize(/** @lends Label# */ {
+    constructor: {
+        value: function Label() {
+            this.super();
+        }
+    }
+});
